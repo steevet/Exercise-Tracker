69c9daa13523bc04e3257d87f9057accb2863faf
steevet <ststeeve067@gmail.com>
2024-05-21 17:35:55 -0400

    May-21

--- mern-tracker/src/App.js
+++ mern-tracker/src/App.js
@@ -1,8 +1,31 @@
+import React from 'react';
+// "npm intall bootstrap" in terminal in main folder so 'Exercise-Tracker'
+//Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. 
+// It contains HTML, CSS and JavaScript-based design templates for typography, forms, buttons, navigation, and other interface components
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// "npm install react-router-dom" in terminal in main folder so 'Exercise-Tracker'
+// This will make it easier to route differnet URLs to different react components
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// Name of different compoents and where they are located
+// "components" folder created, inside 'src'
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExercisesList} /> 
+      <Route path="/edit/:id" component={EditExercise} /> 
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
---
+++ mern-tracker/src/components/navbar.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { link } from 'react-router-dom';
+
+export default class Navbar extends Component{
+    render(){
+        <nav classname="navbar navbar-dark bg-dark navbar-expand-log">
+            
+        </nav>
+    }
+}
